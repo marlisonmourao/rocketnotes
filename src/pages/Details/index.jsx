f6665f116a24d1bc1f25d6521c15281f9de4b938
marlisonmourao <bentesmourao@gmail.com>
2023-03-04 01:17:45 -0400

    applying global styles on the page

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./style"
+
+
+export function Details() {
+  return (
+    <Container>
+      <h1>Hello World</h1>
+      <span>Marlison Mourão</span>
+    </Container>
+  );
+}
