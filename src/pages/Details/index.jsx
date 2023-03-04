cd9c1499c59f1394c0a64d8c6e8c4d3efbff9280
marlisonmourao <bentesmourao@gmail.com>
2023-03-04 14:58:05 -0400

    creating header component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,13 @@
 import { Container } from "./style"
 
 import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World</h1>
-      <span>Marlison Mourão</span>
-
-      <Button />
+      <Header />
+      <Button title="Voltar" />
     </Container>
   );
 }
