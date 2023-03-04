bea04f6426ef800489f5b483e8728cb9dcfb0e3c
marlisonmourao <bentesmourao@gmail.com>
2023-03-04 01:39:59 -0400

    creating button component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,14 @@
 import { Container } from "./style"
 
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World</h1>
       <span>Marlison Mourão</span>
+
+      <Button />
     </Container>
   );
 }
