e306268270984fe8e4bca4acc2197fd1d99ff1df
marlisonmourao <bentesmourao@gmail.com>
2023-03-04 15:31:51 -0400

    bookmarks component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./style";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from '../../components/Tag'
 
 export function Details() {
   return (
@@ -18,6 +19,11 @@ export function Details() {
             <a href="">https://www.rocketseat.com.br/</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="node" />
       </Section>
 
       <Button title="Voltar" />
