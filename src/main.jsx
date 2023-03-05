2e877398cf77742fd3f3cafd5dea75baa8c2dbb5
marlisonmourao <bentesmourao@gmail.com>
2023-03-05 16:49:19 -0400

    building profile interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
