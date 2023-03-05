45603a07abe0b6e83543a594e4ad01b374efaa1a
marlisonmourao <bentesmourao@gmail.com>
2023-03-05 18:02:21 -0400

    building new screen

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,44 @@
+import { Header } from '../../components/Header'
+import { Container, Form } from './styles'
+
+import { Input } from '../../components/Input'
+import { TextArea } from '../../components/TextArea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder="Título" />
+
+          <TextArea placeholder="Observações" />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br" />
+            <NoteItem value="" isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem value="" isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
+        </Form>
+      </main>
+    </Container>
+  )
+}
