069c6a805ca813b9269e25981a5d9a6045d4be1a
marlisonmourao <bentesmourao@gmail.com>
2023-03-08 14:55:09 -0400

    selecting tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,6 +12,18 @@ import { api } from '../../services/api'
 
 export function Home() {
   const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter((tag) => tag !== tagName)
+      setTagsSelected(filteredTags)
+    } else {
+      setTagsSelected((prev) => [...prev, tagName])
+    }
+  }
 
   useEffect(() => {
     async function fetchTags() {
@@ -32,13 +44,21 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText isActive title="Todos" />
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagSelected('all')}
+            isActive={tagsSelected.length === 0}
+          />
         </li>
 
         {tags &&
           tags.map((tag) => (
             <li key={String(tag.id)}>
-              <ButtonText title={tag.name} />
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                isActive={tagsSelected.includes(tag.name)}
+              />
             </li>
           ))}
       </Menu>
