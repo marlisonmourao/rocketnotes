91903ade36abb5958063aee37477131942a8bb27
marlisonmourao <bentesmourao@gmail.com>
2023-03-08 15:13:13 -0400

    filtering notes

--- src/components/Note/index.jsx
+++ src/components/Note/index.jsx
@@ -5,7 +5,7 @@ import { Tag } from '../Tag'
 export function Note({ data, ...rest }) {
   return (
     <Container {...rest}>
-      <h1>{data.title}</h1>
+      <h1>{data.text}</h1>
 
       {data.tags && (
         <footer>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,8 +11,10 @@ import { Note } from '../../components/Note'
 import { api } from '../../services/api'
 
 export function Home() {
+  const [search, setSearch] = useState('')
   const [tags, setTags] = useState([])
   const [tagsSelected, setTagsSelected] = useState([])
+  const [notes, setNotes] = useState([])
 
   function handleTagSelected(tagName) {
     const alreadySelected = tagsSelected.includes(tagName)
@@ -33,6 +35,17 @@ export function Home() {
 
     fetchTags()
   }, [])
+
+  useEffect(() => {
+    async function fetchNotes() {
+      const response = await api.get(
+        `/notes?text=${search}&tags=${tagsSelected}`,
+      )
+      setNotes(response.data)
+    }
+
+    fetchNotes()
+  }, [tagsSelected, search])
 
   return (
     <Container>
@@ -64,20 +77,18 @@ export function Home() {
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+        <Input
+          placeholder="Pesquisar pelo título"
+          onChange={(e) => setSearch(e.target.value)}
+          icon={FiSearch}
+        />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note
-            data={{
-              title: 'React',
-              tags: [
-                { id: '1', name: 'react' },
-                { id: '2', name: 'rocketseat' },
-              ],
-            }}
-          />
+          {notes.map((note) => (
+            <Note key={note.id} data={note} />
+          ))}
         </Section>
       </Content>
 
